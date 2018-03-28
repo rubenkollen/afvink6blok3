1673abdd5a7d8104e6884975aef1f7015660afe1
Ruben <Ruben.kollen@gmail.com>
2018-03-28 15:29:17 +0200

    file opener

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,14 @@
 package main;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.swing.JFileChooser;
 
+public class Main {
+public static String PATHWAY;
+public static String sequentie = "";
 	/**
 	 * @param args
 	 */
@@ -11,7 +18,31 @@ public class Main {
 	}
 	
 	public static String leesBestand(String bestandsNaam){
-		return bestandsNaam;
+		    JFileChooser fileChooser;
+    fileChooser = new JFileChooser();
+        File selectedFile;
+        int reply = fileChooser.showOpenDialog(null);                       //openen van bestand + lezen
+        selectedFile = fileChooser.getSelectedFile();
+        PATHWAY = selectedFile.toString();
+
+        
+        BufferedReader br = null;
+	FileReader fr = null;
+        
+        try{
+        fr = new FileReader(PATHWAY);
+	br = new BufferedReader(fr);
+        String sCurrentLine;
+	while ((sCurrentLine = br.readLine()) != null) {
+            if (sCurrentLine.startsWith(">")){
+                sequentie = "";
+        }
+            else
+                sequentie += sCurrentLine;
+        }	
+        } catch (IOException e) {
+        e.printStackTrace();}
+        return sequentie;
 		
 	} 
 	
